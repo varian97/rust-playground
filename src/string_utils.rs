060fbbc68fba3a598e48cfbbd945c7863629d017
varian97 <varian.caesar@dekoruma.com>
2023-05-01 23:55:45 +0700

    update

--- src/string_utils.rs
+++ src/string_utils.rs
@@ -1,11 +1,14 @@
+pub const VOWELS: [char; 10] = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
+// can also do:
+// pub const VOWELS_SLICE: &[char] = &['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
+
 pub fn convert_to_pig_latin(s: &mut String) {
     let first_letter = match s.chars().nth(0) {
         Some(ch) => ch,
         None => ' ',
     };
 
-    let vowels: Vec<char> = vec!['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'];
-    if vowels.contains(&first_letter) {
+    if VOWELS.contains(&first_letter) {
         s.push_str("-hay");
     } else {
         let first_letter = s.remove(0);
